4bdc4c9242c859c07bc4059380ae317313a434e6
fnassar <fan6236@nyu.edu>
2024-05-02 13:50:24 +0400

    images

---
+++ images01.js
@@ -0,0 +1,26 @@
+images = [
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_1606.JPG")
+  src(s0).out()
+      },
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_0897.JPG")
+  src(s0).out()
+      },
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_1423.JPG")
+  src(s0).out()
+      },
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_1454.JPG")
+  src(s0).out()
+      },
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_1605.JPG")
+  src(s0).out()
+      },
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_3944.JPG")
+  src(s0).out()
+      },
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_3980.JPG")
+  src(s0).out()
+      },
+  ()=>{s0.initImage("https://raw.githubusercontent.com/fnassar/live_coding_final/main/images/IMG_4162.JPG")
+  src(s0).out()
+      }
+]
